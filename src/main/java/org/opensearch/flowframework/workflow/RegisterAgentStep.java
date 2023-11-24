27e51bdd44ccf8d1cdc6309e2852fdcac6768c7a
Owais Kazi <owaiskazi19@gmail.com>
2023-11-23 22:07:05 -0800

    Handled exception for Instant
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>

--- src/main/java/org/opensearch/flowframework/workflow/RegisterAgentStep.java
+++ src/main/java/org/opensearch/flowframework/workflow/RegisterAgentStep.java
@@ -123,10 +123,10 @@ public class RegisterAgentStep implements WorkflowStep {
                         memory = getMLMemorySpec(entry.getValue());
                         break;
                     case CREATED_TIME:
-                        createdTime = (Instant) entry.getValue();
+                        createdTime = getInstant(entry.getValue(), CREATED_TIME);
                         break;
                     case LAST_UPDATED_TIME_FIELD:
-                        lastUpdateTime = (Instant) entry.getValue();
+                        lastUpdateTime = getInstant(entry.getValue(), LAST_UPDATED_TIME_FIELD);
                         break;
                     case APP_TYPE_FIELD:
                         appType = (String) entry.getValue();
@@ -251,6 +251,13 @@ public class RegisterAgentStep implements WorkflowStep {
         // MLMemorySpec mlMemorySpec = builder.build();
         // return mlMemorySpec;
 
+    }
+
+    private Instant getInstant(Object instant, String fieldName) {
+        if (instant instanceof Instant) {
+            return (Instant) instant;
+        }
+        throw new IllegalArgumentException("[" + fieldName + "] must be of type Instant.");
     }
 
 }
